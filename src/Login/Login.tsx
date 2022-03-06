1a2b2d424cc6190c25622a9c94dfebd57f7312bc
MWayneMercado <MWayneMercado@gmail.com>
2022-03-06 13:56:35 -0800

    form setup on login route

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  let nav = useNavigate();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event);
+  }
+
   return (
     <div className="Login">
-      Testing Login
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" id="username" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">Log In</button>
+      </form>
     </div>
   );
 }
