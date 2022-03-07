86adcaefd08ae5e127c05f420ab968e3d9337932
MWayneMercado <MWayneMercado@gmail.com>
2022-03-07 14:27:07 -0800

    enable cors for new localhost port (testing only)

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
   let nav = useNavigate();
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = async () => {
+    let urlParams = new URLSearchParams();
+    urlParams.append('username', username);
+    urlParams.append('password', password);
+
+    try {
+      let response = await fetch(`/admin/login`, {
+        method: "POST",
+        body: urlParams,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        credentials: "include",
+      });
+      const parsedResponse = await response.json();
+      console.log(parsedResponse);
+
+    } catch(err) {
+      console.log('----- Login ERROR -----');
+      console.log(err);
+    }
+  }
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(event);
+    loginUser();
   }
 
   return (
@@ -16,9 +40,9 @@ function Login() {
         onSubmit={handleSubmit} >
         <h1 className="text-xl font-bold mb-2.5">Media Map Login</h1>
         <label htmlFor="username">Username</label>
-        <input className="border px-4 py-1 mb-2.5" type="text" name="username" id="username" />
+        <input className="border px-4 py-1 mb-2.5" type="text" name="username" id="username" onChange={e => setUsername(e.target.value)}/>
         <label htmlFor="password">Password</label>
-        <input className="px-2 py-1 border" type="password" name="password" id="password" />
+        <input className="px-2 py-1 border" type="password" name="password" id="password" onChange={e => setPassword(e.target.value)}/>
         <button 
           className="self-center px-6 py-1 mt-5 font-bold text-green-600 border border-green-600 rounded-full active:scale-95 hover:border-white w-min whitespace-nowrap hover:bg-gradient-to-tr from-green-600 to-green-400 hover:text-white"
           type="submit"
