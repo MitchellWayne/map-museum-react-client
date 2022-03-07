8fc86c907b1ba6d12a3f3bd13c73c74aceb02df0
MWayneMercado <MWayneMercado@gmail.com>
2022-03-07 13:52:42 -0800

    apply headwind opinionated class sorting

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -10,18 +10,17 @@ function Login() {
   }
 
   return (
-    <div className="Login flex h-screen w-screen items-center justify-center">
+    <div className="flex items-center justify-center w-screen h-screen Login">
       <form
-        className="p-6 max-w-sm max-h-min flex flex-col bg-white rounded-md drop-shadow"
+        className="flex flex-col max-w-sm p-6 bg-white rounded-md max-h-min drop-shadow"
         onSubmit={handleSubmit} >
         <h1 className="text-xl font-bold mb-2.5">Media Map Login</h1>
         <label htmlFor="username">Username</label>
         <input className="border px-4 py-1 mb-2.5" type="text" name="username" id="username" />
         <label htmlFor="password">Password</label>
-        <input className="border px-2 py-1" type="password" name="password" id="password" />
+        <input className="px-2 py-1 border" type="password" name="password" id="password" />
         <button 
-          className="w-min whitespace-nowrap px-6 py-1 mt-5 border border-green-600
-          font-bold text-green-600 rounded-full self-center hover:bg-gradient-to-tr from-green-600 to-green-400 hover:text-white"
+          className="self-center px-6 py-1 mt-5 font-bold text-green-600 border border-green-600 rounded-full w-min whitespace-nowrap hover:bg-gradient-to-tr from-green-600 to-green-400 hover:text-white"
           type="submit"
         >
           Log In
