d28fbc3aac4e950109a37248804724e5a2fca8de
MWayneMercado <MWayneMercado@gmail.com>
2022-03-09 12:53:25 -0800

    prep google map loader (no api key)

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
 
 function Map() {
+  let map = null;
+
+  const loader = new Loader({
+    apiKey: "",
+    version: "weekly"
+  });
+
+  loader.load()
+  .then((google) => {
+    map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center: { lat: 45.9795412, lng: -51.6052898 },
+      zoom: 2,
+      fullscreenControl: false,
+      mapTypeControl: false,
+      streetViewControl: false,
+      zoomControl: false,
+      disableDoubleClickZoom: true,
+    });
+  })
+
   return (
     <div className="Map">
       Testing Map
