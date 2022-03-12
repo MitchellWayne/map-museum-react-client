06acc0f2d5f7e8b5af0e7e16fd1da0af25e9912f
MWayneMercado <MWayneMercado@gmail.com>
2022-03-11 16:09:27 -0800

    style target coordinate on form

--- src/NoteForm/NoteForm.tsx
+++ src/NoteForm/NoteForm.tsx
@@ -1,9 +1,16 @@
 import { clientPropState } from '../interfaces';
 
 function NoteForm(props: clientPropState) {
+
   return (
-    <div className="absolute top-0 z-10 h-screen p-2 text-white bg-black/50 NoteForm w-72">
-      {props.latlng}
+    <div className="flex-col items-center absolute top-0 z-10 flex h-screen p-2 text-white bg-black/50 NoteForm w-72">
+      <h2 className="text-center text-sm">
+        <span className='font-bold text-xl'>Create a new note at:</span>
+        {props.latlng}
+      </h2>
+      <form action="">
+        
+      </form>
     </div>
   );
 }
