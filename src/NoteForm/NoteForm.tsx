8e6e0c13327ddf2ab22a3b1cc6f143a5e52f26b2
MWayneMercado <MWayneMercado@gmail.com>
2022-03-13 13:47:01 -0700

    add fontawesome btn to close form

--- src/NoteForm/NoteForm.tsx
+++ src/NoteForm/NoteForm.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import { clientPropState } from '../interfaces';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function NoteForm(props: clientPropState) {
-  const [latlng, setLatlng] = useState(['']);
+  const [latlng, setLatlng] = useState(['']); // For parsing props.latlng
   const [simpleForm, setSimpleForm] = useState(false);
 
   useEffect(() => {
@@ -11,12 +13,13 @@ function NoteForm(props: clientPropState) {
 
   return (
     <div className="absolute top-0 z-10 flex flex-col items-center h-screen text-white bg-black/50 NoteForm w-72">
-      <h2 className="text-sm text-center m-2">
+      <FontAwesomeIcon className="absolute top-0 right-0 mt-1 mr-2 text-2xl hover:text-gray-700" icon={faTimes}/>
+      <h2 className="m-2 text-sm text-center">
         <span className='text-xl font-bold font-gideon-roman'>Create a new note at:</span>
         <div className="text-left">Latitude: {latlng[0]}</div>
         <div className="text-left">Longitude: {latlng[1]}</div>
       </h2>
-      <div className="flex flex-row formToggleWrapper justify-evenly mt-2 w-full font-gideon-roman text-xl">
+      <div className="flex flex-row w-full mt-2 text-xl formToggleWrapper justify-evenly font-gideon-roman">
         <button
           className={`grow ${simpleForm ? 'text-white bg-black hover:bg-gray-700' : 'text-black bg-white'}`}
           onClick={() => {setSimpleForm(false)}}
