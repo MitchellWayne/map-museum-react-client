53262ebeb0be20bbe3174e23d2053a91020cc464
MWayneMercado <MWayneMercado@gmail.com>
2022-03-14 12:56:19 -0700

    add styled submit btn

--- src/SeriesForm/SeriesForm.tsx
+++ src/SeriesForm/SeriesForm.tsx
@@ -19,16 +19,22 @@ function SeriesForm(props: any) {
       </h2>
       
       <form 
-        className="flex flex-col w-full font-semibold font-sans"
+        className="flex flex-col w-full font-sans font-semibold"
         action="">
         <label className="ml-2" htmlFor="name">Series Name</label>
         <input className="pl-2 text-black" type="text" name="name" id="name" />
-        <label className="ml-2 mt-2" htmlFor="desc">Description</label>
+        <label className="mt-2 ml-2" htmlFor="desc">Description</label>
         <textarea className="pl-2 text-black" name="desc" id="desc" rows={2} placeholder="Briefly describe the series"></textarea>
-        <label className="ml-2 mt-2" htmlFor="iconImage">Series Icon Image</label>
-        <input className="ml-2 mt-2" type="file" name="iconImage" id="iconImage" />
-        <label className="ml-2 mt-2" htmlFor="mainImage">Series Main Image</label>
-        <input className="ml-2 mt-2" type="file" name="mainImage" id="mainImage" />
+        <label className="mt-2 ml-2" htmlFor="iconImage">Series Icon Image</label>
+        <input className="mt-2 ml-2" type="file" accept='image/png, image/jpeg' name="iconImage" id="iconImage" />
+        <label className="mt-2 ml-2" htmlFor="mainImage">Series Main Image</label>
+        <input className="mt-2 ml-2" type="file" accept='image/png, image/jpeg' name="mainImage" id="mainImage" />
+        <button 
+          className="self-center mt-5 px-5 text-xl font-bold text-white border border-white rounded-full active:scale-95 hover:border-white w-min whitespace-nowrap hover:bg-gradient-to-tr from-green-600 to-green-400 hover:text-white"
+          type="submit"
+        >
+          Create Series
+        </button>
       </form>
     </div>
   );
