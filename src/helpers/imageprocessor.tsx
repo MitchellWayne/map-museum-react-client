daf3f7acc329340eb10d2eb4144c08c4ffc2052b
MWayneMercado <MWayneMercado@gmail.com>
2022-03-14 14:07:43 -0700

    implement toIcon

--- src/helpers/imageprocessor.tsx
+++ src/helpers/imageprocessor.tsx
@@ -1,9 +1,20 @@
-import Jimp from "jimp/*";
+import Jimp from "jimp";
 
-export function toIcon(file: File){
+export async function toIcon(filepath: string): Promise<Buffer>{
+  let imgbuffer: Buffer = Buffer.from(filepath);
+  await Jimp.read(filepath)
+    .then(async image => {
+      image.cover(100, 100);
+      image.scaleToFit(100, 100);
+      imgbuffer = await image.getBufferAsync(Jimp.MIME_PNG);;
+    })
+    .catch(err => {
+      console.log(err);
+    })
 
+  return imgbuffer;
 }
 
-export function toStandard(file: File){
-
+export async function toStandard(file: File){
+  console.log(file);
 }
