0acdab22a7f69a8dca478a78c1f0a4f9fdded51b
MWayneMercado <MWayneMercado@gmail.com>
2022-03-14 15:06:21 -0700

    image preview testing

--- src/SeriesForm/SeriesForm.tsx
+++ src/SeriesForm/SeriesForm.tsx
@@ -2,19 +2,33 @@ import { useEffect, useState } from 'react';
 // import { clientPropState } from '../interfaces';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { toIcon, toStandard } from '../helpers/imageprocessor';
+// import { toIcon, toStandard } from '../helpers/imageprocessor';
 
 function SeriesForm(props: any) {
   const [name, setName] = useState('');
   const [desc, setDesc] = useState('');
-  const [icon, setIcon] = useState('');
+  const [icon, setIcon] = useState<Blob>();
   const [mainImg, setMainImg] = useState('');
 
-  const [uIcon, setuIcon] = useState(Buffer.from(''));
+  const [uIcon, setuIcon] = useState<string>();
 
   const createSeries = async () => {
-    const updatedIcon = await toIcon(icon);
-    setuIcon(updatedIcon);
+    // const updatedIcon = await toIcon(icon);
+    // setuIcon(updatedIcon);
+    // THIS IS JUST TESTING FOR DISPLAY
+    // DO NOT USE
+    if (icon) {
+      console.log(icon);
+      let fr = new FileReader();
+      fr.readAsDataURL(icon);
+      fr.onload = (e) => {
+        console.log(e);
+        if (e.target && e.target.result) {
+          setuIcon(e.target.result as string);
+          console.log(uIcon);
+        }
+      }
+    }
   }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -41,7 +55,7 @@ function SeriesForm(props: any) {
         <label className="mt-2 ml-2" htmlFor="desc">Description</label>
         <textarea className="pl-2 text-black" name="desc" id="desc" rows={2} placeholder="Briefly describe the series" onChange={e => setDesc(e.target.value)}></textarea>
         <label className="mt-2 ml-2" htmlFor="iconImage">Series Icon Image</label>
-        <input className="mt-2 ml-2" type="file" accept='image/png, image/jpeg' name="iconImage" id="iconImage" onChange={e => setIcon(e.target.value)} />
+        <input className="mt-2 ml-2" type="file" accept='image/png, image/jpeg' name="iconImage" id="iconImage" onChange={e => {if (e.target.files && e.target.files.length > 0) setIcon(e.target.files[0])}} />
         <label className="mt-2 ml-2" htmlFor="mainImage">Series Main Image</label>
         <input className="mt-2 ml-2" type="file" accept='image/png, image/jpeg' name="mainImage" id="mainImage" onChange={e => setMainImg(e.target.value)} />
         <button 
@@ -52,7 +66,7 @@ function SeriesForm(props: any) {
         </button>
       </form>
 
-      <img src={uIcon.toString('base64')} alt="" />
+      <img src={uIcon} alt="" />
     </div>
   );
 }
